b83ed509e93b75320de70e7d8237902c356b03fb
Michael Ocasio <michael.a.ocasiofigueroa@gmail.com>
2018-12-02 23:07:55 -0500

    Addition: PLanetWeight.java
    - This java program sets the initial weight then uses basic math to show the declared weight on different planets in the form of a string and print method.
    -Very Basic, if you wish to replace the wight with your own just change line 7 to your weight.

---
+++ PlanetWeight.java
@@ -0,0 +1,22 @@
+package com.Java24Hrs;
+
+public class PlanetWeight {
+    public static void main (String[] args) {
+
+        System.out.println("Your weight on Earth is ");
+        double weight = 178;
+        System.out.println(weight);
+
+        System.out.println("Your weight on Mercury is ");
+        double mercury = weight * .378;
+        System.out.println(mercury);
+
+        System.out.println("Your weight on the Moon is ");
+        double moon = weight * .166;
+        System.out.println(moon);
+
+        System.out.println("Your weight on Jupiter is ");
+        double jupiter = weight * 2.364;
+        System.out.println(jupiter);
+    }
+}
